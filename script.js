4c1f4ed9ccf097beda0032b3a210710e709e5b51
GabrieladcCespedes <160735888+GabrieladcCespedes@users.noreply.github.com>
2024-04-26 16:14:41 -0400

    Update script.js

--- script.js
+++ script.js
@@ -4,24 +4,20 @@
 se hace click en el por el texto "Logout" y asi viceversa si el texto del boton 
 es "Logout" se camnbia por "Login" */
 function cambiarText(){
-    var text = document.getElementById("btn-login")
-    if (text.innerHTML === "Login"){
-        text.innerHTML = "Logout";
+   /* var text = document.getElementById("btn-login")*/
+    if (text.innerText === "Login"){
+        text.innerText = "Logout";
     } else{
-        text.innerHTML = "Login";
+        text.innerText = "Login";
     }
 }
 /*La funcion removeBtn remueve un boton de la pagina al hacer click en el */
 function removeBtn(element){
     element.remove();
 }
-/*La funcion alertLikes es un contador de likes, por cada click que se le de al boton
-se incrementa el numeros dde likes y se muestra un mensaje de alerta avisando que acabas 
+/*La funcion alertLikes muestra un mensaje de alerta avisando que acabas 
 de darle like*/
-var contador = 0;
 function alertLikes(boton){ 
-    contador ++;
-    boton.innerHTML = contador + " likes";
     alert("This page says Ninja was liked");
 
 }
